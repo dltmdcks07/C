40a495de82b793c29b87570c38c528c7c3315a1d
dltmdcks07 <william070209@gmail.com>
2026-06-23 19:59:18 +0900

    Add files via upload

---
+++ 2026_06_23.cpp
@@ -0,0 +1,121 @@
+//#include <stdio.h>
+//
+//void main()
+//{
+//	printf("100+100");
+//	printf("\n");
+//	printf("%d", 100 + 100);
+//	printf("\n");
+//}
+
+//#include <stdio.h>
+//
+//void main()
+//{
+//	printf("%d", 100, 200);
+//	printf("\n");
+//	printf("%d %d", 100);
+//	printf("\n");
+//} 
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("안녕하세요\n");
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	printf("안녕하세요\n안녕하세요안녕하세요\n");
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("안녕하세요");
+//	return 0;B
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("안녕하세요\n");
+//	printf("안녕하세요");
+//	printf("안녕하세요 \n");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("사과 1개 \n");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int apple = 1;
+//	printf("사과 %d개 \n", apple);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("키보드 3개 \n");
+//	printf("마우스 2개\n");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int keyboard = 3;
+//	int mouse = 2;
+//	printf("키보드: %d개\n", keyboard);
+//	printf("마우스: %d개\n", mouse);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int coin1 = 500;
+//	int coin2 = 1000;
+//	printf("%d + %d = %d\n", coin1, coin2, coin1 + coin2);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	char A;
+//	A = 'A';
+//	printf("A= %c, A =%d\n", A, A);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	double pi;
+//	pi = 3.14;
+//	float e = 2.718;
+//	printf("pi = %.21f, e=%.3f.. \n", pi, e);
+//	return 0;
+//}
+
+#include <stdio.h>
+int main()
+{
+	int r = 5;
+	float pi = 3.14;
+	float a = r * r * pi;
+	float b = 2 * pi * r;
+	printf("%d * %d *%.2f = %.1f", r, r, pi, a);
+	return 0;
+}
